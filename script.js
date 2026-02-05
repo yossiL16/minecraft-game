77a944f135b23f11968c255551b7a46f5b3dd06b
yossi lerer <ylerer770@gmail.com>
2026-02-05 11:04:51 +0200

    fix second variball from no let to let

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let count = {
 }
 
 let cursor = "";
-selectedBlockFromInventory = null;
+let selectedBlockFromInventory = null;
 
 for(let i=1; i<= 960 ;i++){
     if(i <= 320) { createElement("sky") }
